9b241811bbe59646314ad4cb582d0b2f0442f16a
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2025-05-26 08:12:28 -0300

    Pinga o TG

---
+++ src/apiCalls.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+
+export async function autenticarLoginTG() {
+  try {
+    const response = await axios.post(
+      `${process.env.BACKEND_API_URL}/usuario/login`,
+      { login: process.env.LOGIN, senha: process.env.SENHA }
+    );
+    return response.data; // já vem sem senha e _id
+  } catch (error: any) {
+    throw new Error("Erro ao autenticar usuario", error.message);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,59 @@
-import * as nodemailer from 'nodemailer';
-import * as dotenv from 'dotenv';
+import * as nodemailer from "nodemailer";
+import * as dotenv from "dotenv";
+import { autenticarLoginTG } from "./apiCalls";
+import * as express from "express";
+import axios from "axios";
 
 dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+app.get("/", (req, res) => {
+  res.send("Servidor está funcionando");
 });
 
-function enviarEmail() {
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+async function ping() {
+  try {
+    // Pinga a própria API
+    const response = await axios.get("http://localhost:3000/");
+    console.log(response.data);
+
     const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: process.env.EMAIL_TO,
-        subject: 'Lembrete a cada 1 minuto',
-        text: 'Este é um lembrete automático enviado a cada 1 minuto.'
+      from: process.env.EMAIL_USER,
+      to: process.env.EMAIL_TO,
+      subject: "Lembrete a cada 1 minuto",
+      text: "Este é um lembrete automático enviado a cada 1 minuto.",
     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error('Erro ao enviar email:', error);
-        } else {
-            console.log('Email enviado:', info.response);
-        }
-    });
+    // Envia o email
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email enviado:", info.response);
+
+    // Pinga a API do TG
+    const auth = await autenticarLoginTG();
+    console.log("Autenticação de Login TG: ", auth);
+  } catch (error) {
+    console.error("Erro:", error);
+  }
+
+  console.log(
+    "-------------------------------------------------------------------"
+  );
 }
 
-setInterval(enviarEmail, 1 * 60 * 1000);
-enviarEmail(); // Envia o primeiro imediatamente
+console.log("Aplicação iniciada");
+
+setInterval(ping, 1 * 60 * 1000);
+ping(); // Envia o primeiro imediatamente
